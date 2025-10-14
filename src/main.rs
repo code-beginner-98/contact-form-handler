46e5b15c245e015e04d50aa8a56c86c4e6779a1a
code-beginner-98 <92854522+code-beginner-98@users.noreply.github.com>
2025-10-14 09:15:50 +0200

    added max header size, restructured main() and rewrote in prep for struct use.

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,64 @@
 use std::{
-    collections::HashMap, io::{Read, Write}, net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener}
+    collections::HashMap,
+    io::{Read, Write},
+    net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener},
 };
+
+const MAX_HEADER_SIZE: usize = 1_000;
+
 fn main() {
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
     let Ok(listener) = TcpListener::bind(addr) else {
         print!("Error: TcpListener binding failed.");
         return;
     };
+
+    // Spawn new thread for incoming connection
     for thread in listener.incoming() {
         std::thread::spawn(move || {
             println!("handle created");
-            let mut msg = [0; 512];
-            // TODO: Handle incomplete requests/read to buffer, wait for more
+            let mut buf = [0; 512];
+            let mut msg = Vec::new();
+            let position = 0;
+
             match thread {
                 Ok(mut stream) => {
-                    let Ok(_msg_size) = stream.read(&mut msg) else {
-                        print!("Error: reading of incoming packet failed.");
-                        return;
-                    };
-                    // print!("Message: {}", String::from_utf8_lossy(&msg));
-                    let (header, body) = split_http(&msg);
-                    // println!("header: {header:?}");
-                    // println!("body:   {body:?}\n");
-                    let header_map = split_header(header);
-                    let mail_map = match body {
-                        None => HashMap::new(),
-                        Some(body_text) => split_body(body_text)
-                    };
-                    let keys = ["Connection", "Cache-Control", "Host", "Content-Length"];
-                    for key in keys {
-                        match header_map.get(key) {
-                            Some(value) => println!("{key}: {value}"),
-                            None => println!("{key}: no value associated.")
+                    
+                    // read incoming data into msg, until "\r\n\r\n" pattern is detected in msg or until msg hits size limit
+                    loop {
+                        let Ok(_msg_size) = stream.read(&mut buf) else {
+                            
+                            // Send 400 Bad Request
+                            println!("Error: reading of incoming packet failed.");
+                            return;
                         };
-                    };
-                    println!("mail map: {mail_map:?}");
-                    // TODO: Check Body Length
-                    // Send response
+
+                        // write buf into msg and check msg length
+                        msg.push(buf.to_vec());
+                        if &msg.len() > MAX_HEADER_SIZE{
+                            
+                            // Send 400 Bad Request
+                            println!("Error: header max size exceeded, terminating connection.");
+                        }
+
+                        // Check for pattern, terminate loop if found
+                        if split_http_header(&buf) == Some(position) {
+                            break;
+                        }
+                    }
+
+                    // Extract header and body structs from msg
+                    let (header, body) = split_http(msg);
+
+                    // construct smtp from body fields
+
+
+                    // Send success response website
                     let response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello, world!".as_bytes();
+
                     // TODO: Handle incomplete writes
                     stream.write(response).ok();
-                }
+                },
                 Err(e) => print!("This TcpStream errored: {e}"),
             }
         });
@@ -58,7 +76,7 @@ fn split_http(msg: &[u8]) -> (&[u8], Option<&[u8]>) {
             let body = &msg[val + 4..];
             (header, Some(body))
         }
-        None => (msg, None)
+        None => (msg, None),
     }
 }
 
@@ -68,13 +86,11 @@ fn split_header(header: &[u8]) -> HashMap<String, String> {
     let header_text = String::from_utf8_lossy(header);
     // collect into Vector. Note that .split().collect() is easier n the eyes than for line in header_text {lines.push()}
     // Checks for empty lines created by split(), since last line will certainly end in "\r\n", creating an empty line in the vec.
-    let lines =
-        header_text
+    let lines = header_text
         .split("\r\n")
         .filter(|f| !f.is_empty())
         .map(|s| s.to_string())
-        .collect::<Vec<String>>()
-        ;
+        .collect::<Vec<String>>();
     // extract key-value pairs from the 2nd line onwards at ": "
     let mut header_map = HashMap::new();
     for line in lines.iter().skip(1) {
@@ -87,8 +103,7 @@ fn split_header(header: &[u8]) -> HashMap<String, String> {
 
 fn split_body(body: &[u8]) -> HashMap<String, String> {
     let body_text = String::from_utf8_lossy(body);
-    let pairs =
-        body_text
+    let pairs = body_text
         .split("&")
         .map(|s| s.to_string())
         .collect::<Vec<String>>();
