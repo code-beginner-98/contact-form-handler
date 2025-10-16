23367e659182fe13204f85c50de62a4a5a274802
code-beginner-98 <92854522+code-beginner-98@users.noreply.github.com>
2025-10-16 10:24:29 +0200

    added errr struct, added httpheader, message ad smtpmessage structs

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,32 @@
 use std::{
     collections::{HashMap, VecDeque},
+    fmt,
     io::{Read, Write},
-    net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener, TcpStream},
-    string::FromUtf8Error,
+    net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener, TcpStream}
 };
 
-const MAX_HEADER_SIZE: usize = 512*8;
+const MAX_HEADER_SIZE: usize = 512 * 8;
 
-fn main()
-{
+fn main() {
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
-    let Ok(listener) = TcpListener::bind(addr)
-    else
-    {
+    let Ok(listener) = TcpListener::bind(addr) else {
         print!("Error: TcpListener binding failed.");
         return;
     };
 
     // Spawn new thread for incoming connection
-    for thread in listener.incoming()
-    {
+    for thread in listener.incoming() {
         std::thread::spawn(move || {
             println!("handle created");
             let mut buf = [0; 512];
             let mut msg = Vec::new();
 
-            match thread
-            {
-                Ok(mut stream) =>
-                {
+            match thread {
+                Ok(mut stream) => {
                     // read incoming data into msg, until "\r\n\r\n" pattern is detected in msg or until msg hits size limit
                     let mut pos: Option<usize> = None;
-                    while pos == None
-                    {
-                        let Ok(_msg_size) = stream.read(&mut buf)
-                        else
-                        {
+                    while pos == None {
+                        let Ok(_msg_size) = stream.read(&mut buf) else {
                             // Send 400 Bad Request
                             println!("Error: reading of incoming packet failed.");
                             bad_request(&stream);
@@ -47,8 +38,7 @@ fn main()
 
                         // write buf into msg and check msg length
                         msg.append(&mut buf.to_vec());
-                        if msg.len() > MAX_HEADER_SIZE
-                        {
+                        if msg.len() > MAX_HEADER_SIZE {
                             // Send 400 Bad Request
                             println!("Error: header max size exceeded, terminating connection.");
                             bad_request(&stream);
@@ -58,9 +48,7 @@ fn main()
 
                     // Pattern found, trim front of message, move to header vec, construct header
                     // trim msg and construct header
-                    let Ok((header, trail)) = split_header_from(msg)
-                    else
-                    {
+                    let Ok((header, trail)) = split_header_from(msg) else {
                         // Send 400 Bad Request
                         println!("Error: header content corrupted.");
                         bad_request(&stream);
@@ -82,9 +70,7 @@ fn main()
 
                     // read from buf until msg > content length
                     while msg.len() < content_length {
-                        let Ok(_msg_size) = stream.read(&mut buf)
-                        else
-                        {
+                        let Ok(_msg_size) = stream.read(&mut buf) else {
                             // Send 400 Bad Request
                             println!("Error: reading of incoming packet failed.");
                             bad_request(&stream);
@@ -95,9 +81,7 @@ fn main()
                     }
 
                     // extract body information
-                    let Ok(contact_data) = split_body_from(msg)
-                    else
-                    {
+                    let Ok(contact_data) = split_body_from(msg) else {
                         // Send 400 Bad Request
                         println!("Error: reading of body failed.");
                         bad_request(&stream);
@@ -105,6 +89,7 @@ fn main()
                     };
 
                     // construct smtp from body fields
+                    let smtp_message = SmtpMessage::from(contact_data);
 
                     // Send success response website
                     let response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello, world!".as_bytes();
@@ -117,42 +102,35 @@ fn main()
 }
 
 /// Takes a http-conform header buffer and returns the position of the Empty Line "\r\n\r\n", if present.
-fn check_http_header(buf: &[u8]) -> Option<usize>
-{
+fn check_http_header(buf: &[u8]) -> Option<usize> {
     buf.windows(4).position(|window| window == b"\r\n\r\n")
 }
 
 /// Takes A HTTP-conform msg and returns the header and trailing content after the empty line.
-fn split_header_from(msg: Vec<u8>) -> Result<(HttpHeader, Vec<u8>), &'static str>
-{
+fn split_header_from(msg: Vec<u8>) -> Result<(HttpHeader, Vec<u8>), &'static str> {
     let pos = msg.windows(4).position(|window| window == b"\r\n\r\n");
     let mut header = Vec::new();
     let mut trail = Vec::new();
 
     match pos {
-        Some(val) =>
-        {
+        Some(val) => {
             header = msg[..val].to_vec();
             trail = msg[val + 4..].to_vec();
         }
 
-        None =>
-        {
+        None => {
             let e = "Error: Message got corrupted at runtime.";
             return Err(e);
         }
     }
 
     // form header struct
-    let Ok((start_line, content)) = split_header(header)
-    else
-    {
+    let Ok((start_line, content)) = split_header(header) else {
         let e = "Error: Message got corrupted at runtime";
         return Err(e);
     };
 
-    let http_header = HttpHeader
-    {
+    let http_header = HttpHeader {
         start_line: start_line,
         content: content,
     };
@@ -161,8 +139,7 @@ fn split_header_from(msg: Vec<u8>) -> Result<(HttpHeader, Vec<u8>), &'static str
 }
 
 /// Takes in header slice and turns them into a key-value map for each line
-fn split_header(header: Vec<u8>) -> Result<(Vec<String>, HashMap<String, String>), FromUtf8Error>
-{
+fn split_header(header: Vec<u8>) -> Result<(Vec<String>, HashMap<String, String>), Error> {
     // convert header bytes to text
     let header_text = String::from_utf8(header)?;
 
@@ -194,7 +171,7 @@ fn split_header(header: Vec<u8>) -> Result<(Vec<String>, HashMap<String, String>
     Ok((start_vec, header_map))
 }
 
-fn split_body_from(msg: Vec<u8>) -> Result<HashMap<String, String>, FromUtf8Error> {
+fn split_body_from(msg: Vec<u8>) -> Result<HttpMessage, Error> {
     let body = String::from_utf8(msg)?;
     let pairs = body
         .split("&")
@@ -205,12 +182,92 @@ fn split_body_from(msg: Vec<u8>) -> Result<HashMap<String, String>, FromUtf8Erro
         let (key, value) = pair.split_once("=").unwrap(); // TODO: Error Handling
         mail_map.insert(key.trim().to_string(), value.trim().to_string());
     }
-    Ok(mail_map)
+
+    // construct HttpMessage
+    let http_message = HttpMessage::from(mail_map)?;
+
+    Ok(http_message)
 }
 
 struct HttpHeader {
     start_line: Vec<String>,
     content: HashMap<String, String>,
+}
+
+struct HttpMessage {
+    from_mail: String,
+    surname: String,
+    last_name: String,
+    subject: String,
+    content: String,
+}
+
+impl HttpMessage {
+    fn from(mail_map: HashMap<String, String>) -> Result<HttpMessage, Error> {
+        let (from_mail, surname, last_name, subject, content) = (
+            mail_map.get("email").ok_or("missing email field")?.to_string(),
+            mail_map.get("surname").ok_or("missing surname field")?.to_string(),
+            mail_map.get("last_name").ok_or("missing last_name field")?.to_string(),
+            mail_map.get("subject").ok_or("missing subject field")?.to_string(),
+            mail_map.get("content").ok_or("missing content field")?.to_string(),
+        );
+
+        // read map into fields
+        // TODO: Contact form variable names, structure, fields? This is kind of a placeholder for now.
+        Ok(
+            HttpMessage {
+                from_mail: from_mail,
+                surname: surname,
+                last_name: last_name,
+                subject: subject,
+                content: content,
+            }
+        )
+    }
+}
+
+#[derive(Debug)]
+enum Error {
+    HttpError(String),
+    Utf8Error(String),
+    StringError(String),
+}
+
+impl std::error::Error for Error {}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::HttpError(error_msg) => write!(f, "Error: HttpError: {error_msg}"),
+            Error::Utf8Error(error_msg) => write!(f, "Error: HttpError: {error_msg}"),
+            Error::StringError(error_msg) => write!(f, "Error: HttpError: {error_msg}"),
+        }
+    }
+}
+
+impl From<std::string::FromUtf8Error> for Error
+{
+    fn from(e: std::string::FromUtf8Error) -> Self
+    {
+        Error::Utf8Error(e.to_string())
+    }
+}
+
+impl From<&str> for Error
+{
+    fn from(e: &str) -> Self
+    {
+        Error::StringError(e.to_string())
+    }
+}
+
+struct SmtpMessage {}
+
+impl SmtpMessage {
+    fn from(http_message: HttpMessage) -> Self
+    {
+        
+    }
 }
 
 fn bad_request(mut stream: &TcpStream) {
